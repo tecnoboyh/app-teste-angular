c0c00eb8e2e8b7e54d38d9676b1dd68edd3d29ac
Vinicius H.A.S <viniciushenriquetutos@gmail.com>
2021-11-16 13:48:44 -0300

    Modulo Z

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ZComponent } from './modulo/z/z.component';
+import { C1Component } from './modulo/c1/c1.component';
+import { C2Component } from './modulo/c2/c2.component';
+import { C3Component } from './modulo/c3/c3.component';
 
 const routes: Routes = [];
 
---
+++ src/app/modulo/z/z.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-z',
+  templateUrl: './z.component.html',
+  styleUrls: ['./z.component.css']
+})
+export class ZComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
